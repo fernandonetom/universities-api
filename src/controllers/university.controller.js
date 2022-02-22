de286ede62ba1ad3b05ccbd40e3b73665ca18859
Fernando <fernando39357@gmail.com>
2022-02-22 13:43:32 -0300

    feat: insert, update and remove a university

--- src/controllers/university.controller.js
+++ src/controllers/university.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const { validationResult } = require('express-validator');
 const University = require('../models/university');
 const { createError } = require('../utils/createErrors');
@@ -30,8 +31,8 @@ class UniversityController {
       const totalOfItems = await cursor.count();
       const totalOfPages = Math.ceil(totalOfItems / limit);
 
-      const data = await cursor.skip(20)
-        .limit(10).toArray();
+      const data = await cursor.skip((limit * (page - 1)))
+        .limit(limit).toArray();
 
       return res.json({
         query: {
@@ -60,6 +61,105 @@ class UniversityController {
       if (!university) return next(createError(404));
 
       return res.json(university);
+    } catch (error) {
+      return next(createError(500));
+    }
+  }
+
+  async create(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(createError(422, errors.array()));
+    }
+
+    try {
+      const {
+        domains, country, web_pages, name, alpha_two_code,
+      } = req.body;
+      const state = req.body['state-province'];
+
+      const filter = {
+        country: { $regex: new RegExp(country, 'i') },
+        name: { $regex: new RegExp(name, 'i') },
+      };
+
+      if (state) filter['state-province'] = { $regex: new RegExp(state, 'i') };
+
+      const university = await University.findOne(filter);
+
+      if (university) return next(createError(400, 'University already exists'));
+
+      const created = await University.create({
+        domains,
+        country,
+        web_pages,
+        name,
+        alpha_two_code,
+        'state-province': state,
+      });
+
+      return res.status(201).json(created);
+    } catch (error) {
+      return next(createError(500));
+    }
+  }
+
+  async update(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(createError(422, errors.array()));
+    }
+
+    try {
+      const { id } = req.params;
+
+      const {
+        domains, country, web_pages, name, alpha_two_code,
+      } = req.body;
+      const state = req.body['state-province'];
+
+      const filter = {
+        country: { $regex: new RegExp(country, 'i') },
+        name: { $regex: new RegExp(name, 'i') },
+      };
+
+      if (state) filter['state-province'] = { $regex: new RegExp(state, 'i') };
+
+      const university = await University.findOne(filter);
+
+      if (university && university.id !== id) return next(createError(400, 'University already exists'));
+
+      const updated = await University.findByIdAndUpdate(id, {
+        domains,
+        country,
+        web_pages,
+        name,
+        alpha_two_code,
+        'state-province': state,
+      }, {
+        new: true,
+      });
+
+      return res.status(201).json(updated);
+    } catch (error) {
+      return next(createError(500));
+    }
+  }
+
+  async destroy(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(createError(422, errors.array()));
+    }
+
+    try {
+      const { id } = req.params;
+
+      const university = await University.findByIdAndRemove(id);
+
+      if (!university) return next(createError(404));
+
+      return res.sendStatus(204);
     } catch (error) {
       return next(createError(500));
     }
--- src/routes/university.routes.js
+++ src/routes/university.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { query, param } = require('express-validator');
+const { query, param, check } = require('express-validator');
 const universityController = require('../controllers/university.controller');
 
 const UniversityRoutes = express.Router();
@@ -12,5 +12,32 @@ UniversityRoutes.get('/', [
 UniversityRoutes.get('/:id', [
   param('id').isString(),
   universityController.show]);
+
+UniversityRoutes.delete('/:id', [
+  param('id').isString(),
+  universityController.destroy]);
+
+UniversityRoutes.put('/:id', [
+  param('id').isString(),
+  check('state-province').isString().optional(),
+  check('domains').isArray().optional(),
+  check('domains.*').isString().optional(),
+  check('country').isString().optional(),
+  check('web_pages').isArray().optional(),
+  check('web_pages.*').isString().optional(),
+  check('name').isString().optional(),
+  check('alpha_two_code').isString().optional(),
+  universityController.update]);
+
+UniversityRoutes.post('/', [
+  check('state-province').isString().optional(),
+  check('domains').isArray().optional(),
+  check('domains.*').isString().optional(),
+  check('country').isString(),
+  check('web_pages').isArray().optional(),
+  check('web_pages.*').isString().optional(),
+  check('name').isString(),
+  check('alpha_two_code').isString(),
+  universityController.create]);
 
 module.exports = UniversityRoutes;
